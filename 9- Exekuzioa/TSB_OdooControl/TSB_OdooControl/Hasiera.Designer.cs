931319245782e251c247d6e1d4137a94c57de17d
anderibanez123 <anderibanez123@gmail.com>
2023-10-27 10:45:05 +0200

    programa

---
+++ 9- Exekuzioa/TSB_OdooControl/TSB_OdooControl/Hasiera.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace TSB_OdooControl
+{
+    partial class Hasiera
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // Hasiera
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = Properties.Resources.FondoConClaro;
+            BackgroundImageLayout = ImageLayout.Zoom;
+            ClientSize = new Size(1007, 573);
+            DoubleBuffered = true;
+            Name = "Hasiera";
+            Text = "HASIERA | OdooControl";
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
---
+++ 9- Exekuzioa/TSB_OdooControl/TSB_OdooControl/Hasiera.cs
@@ -0,0 +1,10 @@
+namespace TSB_OdooControl
+{
+    public partial class Hasiera : Form
+    {
+        public Hasiera()
+        {
+            InitializeComponent();
+        }
+    }
+}
